6d5db12c31eba7ed11beae1abbea4a577e1869b9
azer <azer.xu@gmail.com>
2023-05-22 17:24:23 +0800

    添加新词库

--- run.cmd
+++ run.cmd
@@ -4,9 +4,13 @@ sort pybase.org | uniq > tt
 mv tt pybase.org
 
 cat org/pyPhrase.org >> pyphrase.org
+cat third/*.org >> pyphrase.org
+cat scels/*.org >> pyphrase.org
 python yaml.dict.to.org.py phrase/THUOCL_*.dict.yaml
 python yaml.dict.to.org.py phrase/sogou_new_words.dict.yaml
 python yaml.dict.to.org.py -1000 phrase/clover.phrase.dict.yaml
 sort pyphrase.org | uniq > tt
 mv tt pyphrase.org
 createPYMB pybase.org pyphrase.org
+
+cp *.mb ~/.config/fcitx/pinyin/
---
+++ scels/run.cmd
@@ -0,0 +1,1 @@
+for i in `ls *.scel`;do scel2org ${i} > ${i}.org; done
